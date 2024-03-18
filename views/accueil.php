2fa20f01b0f48d639e6115830587655e0233ff9a
WITTMER <thomaswittmer@outlook.fr>
2024-03-18 14:13:06 +0100

    Ajout du htaccess et du index.php

---
+++ views/accueil.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Titre de la page d'accueil</title>
+</head>
+<body>
+    <!-- Contenu de la page d'accueil -->
+    <h1>Bienvenue sur notre site</h1>
+    <p>Ceci est une page d'accueil simple.</p>
+</body>
+</html>
