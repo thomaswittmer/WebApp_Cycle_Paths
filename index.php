f7c96915e750eb43ba618ec568bbe27fbe940b37
Ienissei <bounanjeanne@gmail.com>
2024-03-20 16:55:31 +0100

    architecture de la page et appels par des fetch aux annees et luminosité de la bdd avec click (mais on a pas encore la bdd)

--- index.php
+++ index.php
@@ -6,6 +6,22 @@ Flight::route('/', function(){
     Flight::render('accueil');
 });
 
+Flight::route('POST /lumino', function(){
+    $test = null;
+    if (isset($_POST['lumi'])){
+        $test = "luminosité : ".$_POST['lumi'];
+    }
+    Flight::json($test);
+});
+
+Flight::route('POST /recup_annee', function(){
+    $test = null;
+    if (isset($_POST['annee'])){
+        $test = "année : ".$_POST['annee'];
+    }
+    Flight::json($test);
+});
+
 Flight::start();
 
 ?>
--- views/accueil.php
+++ views/accueil.php
@@ -8,24 +8,26 @@
 </head>
 <body>
     <div id="app">
+
+        <!-- Barre latéral pour choisir les paramètres -->
+        <div class="barre_laterale">
+            <form id="Lumi" method='POST' action=''>
+                Type de luminosité :<br>
+                <input type="radio" name="lum" value="jour"> Jour<br>
+                <input type="radio" name="lum" value="nuit_avec"> Nuit avec éclairage<br>
+                <input type="radio" name="lum" value="nuit_sans"> Nuit sans éclairage<br>
+            </form>
+        </div>
+
         <!-- Contenu de la page d'accueil -->
         <div class="carte">
             <p>Ceci est une page d'accueil simple.</p>
-        </div>
-    <!-- curseur temporel -->
-        <div class="curseur-date">
-                <input type="range" min="2000" max="2022" v-model="selectedYear" id="dateSlider">
-                <p>Date sélectionnée : {{ selectedYear }}</p>
-        </div>
-        <!-- Barre latéral pour choisir les paramètres -->
-        <div class="barre_laterale">
-            <form method='POST' action=''>
-            Type de luminosité :<br>
-            <input type="radio" name="lum" value="jour"> Jour<br>
-            <input type="radio" name="lum" value="nuit_avec"> Nuit avec éclairage<br>
-            <input type="radio" name="lum" value="nuit_sans"> Nuit sans éclairage<br>
-            <input type="submit" name="exple2" value="Sélectionner">
-            </form>
+
+            <!-- curseur temporel -->
+            <div class="curseur-date">
+                    <input type="range" min="2000" max="2022" v-model="selectedYear" id="dateSlider" @change="cherche_annee">
+                    <p>Date sélectionnée : {{ selectedYear }}</p>
+            </div>
         </div>
     </div>
     <script src="assets/accueil.js"></script>
