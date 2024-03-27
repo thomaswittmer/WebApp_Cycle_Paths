30c31964819ff05a945a023acd34a2b67194c283
Ienissei <bounanjeanne@gmail.com>
2024-03-27 14:59:22 +0100

    nouvelles checkboxes

--- views/map2.php
+++ views/map2.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="assets/map_style.css">
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     
@@ -19,9 +20,9 @@
         <div class="barre_laterale">
             <form id="Lumi" method='POST' action=''>
                 Type de luminosité :<br>
-                <input type="radio" name="lum" value="jour"> Jour<br>
-                <input type="radio" name="lum" value="nuit_avec"> Nuit avec éclairage<br>
-                <input type="radio" name="lum" value="nuit_sans"> Nuit sans éclairage<br>
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value="jour"> Jour<br>
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value="nuit_avec"> Nuit avec éclairage<br>
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value="nuit_sans"> Nuit sans éclairage<br>
             </form>
         </div>
 
